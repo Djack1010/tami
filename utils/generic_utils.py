000446359fec3f0ed1f7c3f05e28c76197e19734
djack1010 <giachi.iada@gmail.com>
2020-11-04 23:15:36 +0100

    adding install.sh to guide the setup

--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -8,7 +8,7 @@ def print_log(string, print_on_screen=False, print_on_file=True):
     if print_on_screen:
         print(string)
     if print_on_file:
-        with open(main_path + 'results/ex_logs/' + timeExec + ".results", 'a') as logfile:
+        with open(main_path + 'results/exec_logs/' + timeExec + ".results", 'a') as logfile:
             logfile.write(string + "\n")
 
 
