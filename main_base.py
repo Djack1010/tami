7a28e2e1c9d9dc996d5e7cc31c963eeb56cb129b
djack1010 <giachi.iada@gmail.com>
2020-11-06 20:08:19 +0100

    code refactoring: make main_base.py ligther and moved most of the functions to handle_modes.py.

--- main_base.py
+++ main_base.py
@@ -6,123 +6,12 @@ import numpy as np
 import pathlib
 import tensorflow as tf
 from models_base.basic import BASIC
+from models_base.nedo import NEDO
 from models_base.VGG16 import VGG16_19
 from utils.config import *
 from utils.generic_utils import print_log
-from utils.analyzing_data import multiclass_analysis
+import utils.handle_modes as modes
 from utils.preprocessing_data import get_info_dataset
-
-
-def main(arguments, class_info):
-
-    print_log("STARTING EXECUTION AT\t{}".format(time.strftime("%d-%m %H:%M:%S")), print_on_screen=True)
-    print("LOADING AND PRE-PROCESSING DATA")
-
-    dataset_base = main_path + arguments.dataset
-
-    # Create dataset of filepaths
-    train_paths_ds = tf.data.Dataset.list_files(dataset_base + "/training/train/*/*")
-    val_paths_ds = tf.data.Dataset.list_files(dataset_base + "/training/val/*/*")
-    final_training_paths_ds = tf.data.Dataset.list_files(dataset_base + "/training/*/*/*")
-    test_paths_ds = tf.data.Dataset.list_files(dataset_base + "/test/*/*")
-
-    # STATS
-    size_train = class_info['train_size']
-    size_val = class_info['val_size']
-    size_test = class_info['test_size']
-    class_names = class_info['class_names']
-    nclasses = class_info['n_classes']
-
-    # SELECTING MODELS
-    model = _model_selection(arguments, nclasses)
-
-    # Print information on log
-    # EXECUTION Info
-    print_log("INFO EXECUTION:"
-              "\nmodel = {}\ndataset = {}"
-              "\noutput_model = {}\nepochs = {}\nbatch_size = {}\ncaching = {}"
-              "\n----------------"
-              .format(arguments.model, arguments.dataset,
-                      arguments.output_model, arguments.epochs, arguments.batch_size, arguments.caching))
-
-    # DATA Info
-    print_log("INFO DATA:"
-              "\num_classes = {}\nclass_names= {}\nnsize_img= {}x{}\nSize train-val-test= {}-{}-{}"
-              .format(nclasses, class_names, arguments.image_size, arguments.channels, size_train, size_val, size_test))
-    for ds_class in class_names:
-        print_log("{} : {}-{}-{} -> {}".format(ds_class, class_info['info'][ds_class]['TRAIN'],
-                                               class_info['info'][ds_class]['VAL'],
-                                               class_info['info'][ds_class]['TEST'],
-                                               class_info['info'][ds_class]['TOT']))
-    print_log("----------------")
-
-    # --------------  TRAINING and VALIDATION part  --------------------
-
-    #  Use Dataset.map to create a dataset of image, label pairs
-    # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
-    lab_train_ds = train_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    lab_val_ds = val_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-
-    # Caching dataset in memory for big dataset (IF arguments.caching is set)
-    train_ds, val_ds = prepare_ds(arguments.caching, lab_train_ds, lab_val_ds, "train", "val", arguments.batch_size)
-
-    print_log('Start Training for {} epochs  '.format(arguments.epochs), print_on_screen=True)
-
-    # Initialize callbacks for Tensorboard
-    log_fit = "tensorboard_logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback_fit = tf.keras.callbacks.TensorBoard(log_dir=log_fit, histogram_freq=1)
-
-    train_results = model.fit(x=train_ds, batch_size=arguments.batch_size, epochs=arguments.epochs,
-                              validation_data=val_ds, callbacks=[tensorboard_callback_fit])
-
-    print_log("\ttraining loss: {} \n\ttraining acc:{} \n\tvalidation loss:{} \n\tvalidation acc:{}"
-              .format(train_results.history['loss'], train_results.history['acc'],
-                      train_results.history['val_loss'], train_results.history['val_acc']))
-
-    del train_ds, val_ds
-
-    # --------------  FINAL TRAINING and TEST part  --------------------
-
-    #  Use Dataset.map to create a dataset of image, label pairs
-    # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
-    lab_final_train_ds = final_training_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    lab_test_ds = test_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-
-    # NB The batch_val_test is set to 1 to make easier the calculation of the performance results
-    fin_train_ds, test_ds = prepare_ds(arguments.caching, lab_final_train_ds, lab_test_ds, "fin_tr", "test",
-                                       arguments.batch_size, batch_val_test=1)
-
-    # Train the model over the entire total_training set and then test
-    print_log('Start Final Training for {} epochs  '.format(arguments.epochs), print_on_screen=True)
-    start_training = time.perf_counter()
-    final_train_results = model.fit(x=fin_train_ds, batch_size=arguments.batch_size, epochs=arguments.epochs)
-    end_training = time.perf_counter()
-    print_log("\ttraining loss: {} \n\ttraining acc:{}" .format(final_train_results.history['loss'],
-                                                                final_train_results.history['acc']))
-    print_log("FINAL TRAINING TIME: {} ".format(str(datetime.timedelta(seconds=end_training - start_training))))
-
-    # Test the trained model over the test set
-    print_log('Start Test', print_on_screen=True)
-    results = model.evaluate(test_ds)
-    print_log("\ttest loss: {} \n\ttest accuracy: {}".format(results[0], results[1]), print_on_screen=True)
-    print_log("\tPrec: {} \n\tRecall: {}".format(results[2], results[3]), print_on_screen=True)
-    # F-measure calculated as (2 * Prec * Recall)/(Prec + Recall)
-    print_log("\tF-Measure: {} \n\tAUC: {}".format((2*results[2]*results[3])/(results[2]+results[3]), results[4]),
-              print_on_screen=True)
-
-    cm, results_classes, to_print = multiclass_analysis(model, test_ds, class_names,
-                                                        save_fig=main_path + "results/figures/CM_{}".format(timeExec))
-    print_log("Results per classes", print_on_screen=True)
-    print_log(to_print, print_on_screen=True)
-
-    del fin_train_ds, test_ds
-
-    # save model and architecture to single file
-    if arguments.output_model is not None:
-        model.save(main_path + 'model_saved/{}_m{}'.format(arguments.output_model, arguments.model))
-        model.save_weights(main_path + 'model_saved/{}_m{}_weights'.format(arguments.output_model, arguments.model))
-
-    print_log("ENDING EXECUTION AT\t{}".format(time.strftime("%d-%m %H:%M:%S")), print_on_screen=True)
 
 
 def parse_args():
@@ -131,12 +20,16 @@ def parse_args():
     group = parser.add_argument_group('Arguments')
     # REQUIRED Arguments
     group.add_argument('-m', '--model', required=True, type=str,
-                       help='BASIC or VGG16')
+                       help='BASIC, NEDO or VGG16')
     group.add_argument('-d', '--dataset', required=True, type=str,
                        help='the dataset to be used')
     # OPTIONAL Arguments
     group.add_argument('-o', '--output_model', required=False, type=str, default=None,
                        help='Name of model to output and store')
+    group.add_argument('-l', '--load_model', required=False, type=str, default=None,
+                       help='Name of model to load')
+    group.add_argument('-t', '--tuning', required=False, type=str, default=None,
+                       help='Run Keras Tuner for choosing hyperparameters [hyperband, random, bayesian]')
     group.add_argument('-e', '--epochs', required=False, type=int, default=10,
                        help='number of epochs')
     group.add_argument('-b', '--batch_size', required=False, type=int, default=32)
@@ -146,6 +39,8 @@ def parse_args():
     group.add_argument('-w', '--weights', required=False, type=str, default=None,
                        help="If you do not want random initialization of the model weights "
                             "(ex. 'imagenet' or path to weights to be loaded)")
+    group.add_argument('--mode', required=False, type=str, default='training',
+                       help="Choose which mode run between 'training' (default), 'test'")
     # FLAGS
     group.add_argument('--exclude_top', dest='include_top', action='store_false',
                        help='Exclute the fully-connected layer at the top pf the network (default INCLUDE)')
@@ -158,8 +53,7 @@ def parse_args():
 
 
 def _check_args(arguments):
-    if arguments.model != "BASIC" \
-            and arguments.model != "VGG16":
+    if arguments.model != "BASIC" and arguments.model != "NEDO" and arguments.model != "VGG16":
         print('Invalid model choice, exiting...')
         exit()
     if re.match(r"^\d{2,4}x([13])$", arguments.image_size):
@@ -180,103 +74,35 @@ def _check_args(arguments):
         print("Dataset '{}' should contain folders 'test, training/train and training/val'...".format(
             arguments.dataset))
         exit()
+    if arguments.mode != "training" and arguments.mode != "test":
+        print('Invalid mode choice, exiting...')
+        exit()
+    elif arguments.tuning is not None and arguments.tuning != 'hyperband' and arguments.tuning != 'random' \
+            and arguments.tuning != 'bayesian':
+        print('Invalid tuning choice, exiting...')
+        exit()
 
 
 def _model_selection(arguments, nclasses):
-    model = None
     print("INITIALIZING MODEL")
-
+    mod_class = None
     if arguments.model == "BASIC":
-        model_class = BASIC(nclasses, arguments.image_size, arguments.channels)
+        mod_class = BASIC(nclasses, arguments.image_size, arguments.channels)
+    elif arguments.model == "NEDO":
+        mod_class = NEDO(nclasses, arguments.image_size, arguments.channels)
     elif arguments.model == "VGG16":
         # NB. Setting include_top=True and thus accepting the entire struct, the input Shape MUST be 224x224x3
         # and in any case, channels has to be 3
         if arguments.channels != 3:
             print("VGG requires images with channels 3, please set --image_size <YOUR_IMAGE_SIZE>x3, exiting...")
             exit()
-        model_class = VGG16_19(nclasses, arguments.image_size, arguments.channels,
-                      weights=arguments.weights, include_top=arguments.include_top)
+        mod_class = VGG16_19(nclasses, arguments.image_size, arguments.channels,
+                               weights=arguments.weights, include_top=arguments.include_top)
     else:
         print("model {} not implemented yet...".format(arguments.model))
         exit()
 
-    model = model_class.build()
-    return model
-
-
-def get_label(file_path):
-    # convert the path to a list of path components
-    parts = tf.strings.split(file_path, os.path.sep)
-    # The second to last is the class-directory
-    # cast to float32 for one_hot encode (otherwise TRUE/FALSE tensor)
-    return tf.cast(parts[-2] == CLASS_NAMES, tf.float32)
-
-
-def decode_img(img):
-    # convert the compressed string to a 3D uint8 tensor
-    img = tf.image.decode_png(img, channels=CHANNELS)  # tf.image.decode_jpeg(img, channels=CHANNELS)
-    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    # resize the image to the desired size.
-    return tf.image.resize(img, [IMG_DIM, IMG_DIM])
-
-
-def process_path(file_path):
-    label = get_label(file_path)
-    # load the raw data from the file as a string
-    img = tf.io.read_file(file_path)
-    img = decode_img(img)
-    return img, label
-
-
-def prepare_for_training(ds, batch_size, cache=True, shuffle_buffer_size=1000, loop=False):
-    """
-    cache:  If isinstance(cache, str), then represents the name of a
-            directory on the filesystem to use for caching elements in this Dataset.
-            Otherwise, the dataset will be cached in memory.
-    """
-    # IF it is a small dataset, only load it once and keep it in memory.
-    # OTHERWISE use `.cache(filename)` to cache preprocessing work for datasets that don't fit in memory.
-    if cache:
-        if isinstance(cache, str):
-            ds = ds.cache(cache)
-        else:
-            ds = ds.cache()
-
-    ds = ds.shuffle(buffer_size=shuffle_buffer_size)
-
-    # Repeat forever
-    if loop:
-        ds = ds.repeat()
-
-    ds = ds.batch(batch_size)
-
-    # `prefetch` lets the dataset fetch batches in the background while the model
-    # is training.
-    ds = ds.prefetch(buffer_size=AUTOTUNE)
-
-    return ds
-
-
-def prepare_ds(caching, train, val_test, cache_train, cache_val_test, batch_train, batch_val_test=None):
-    if batch_val_test is None:
-        batch_val_test = batch_train
-    if caching:
-        # delete previous cache files and store for this execution
-        caching_file_base = main_path + "temp/"
-        for f in os.listdir(caching_file_base):
-            if "{}.tfcache".format(cache_train) in f or "{}.tfcache".format(cache_val_test) in f:
-                os.remove(caching_file_base + f)
-        train_ds = prepare_for_training(train, batch_size=batch_train,
-                                        cache=caching_file_base + "{}.tfcache".format(cache_train))
-        test_val_ds = prepare_for_training(val_test, batch_size=batch_val_test,
-                                           cache=caching_file_base + "{}.tfcache".format(cache_val_test))
-
-    else:
-        train_ds = prepare_for_training(train, batch_size=batch_train)
-        test_val_ds = prepare_for_training(val_test, batch_size=batch_val_test)
-
-    return train_ds, test_val_ds
+    return mod_class
 
 
 if __name__ == '__main__':
@@ -288,17 +114,12 @@ if __name__ == '__main__':
     # STRUCT of class_info = {'class_names': np.array(string), 'n_classes': int,
     # "train_size": int, "val_size": int, "test_size": int, 'info': dict}
     # for name in class_info['class_names'] the info dict contains = {'TRAIN': int, 'VAL': int, 'TEST': int, 'TOT': int}
-    class_info = get_info_dataset(args.dataset)
+    class_info, ds_info = get_info_dataset(args.dataset)
 
     # GLOBAL SETTINGS FOR THE EXECUTIONS
     # Reduce verbosity for Tensorflow Warnings and set dtype for layers
     # tf.keras.backend.set_floatx('float64')
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-    CHANNELS = args.channels
-    IMG_DIM = args.image_size
-    CLASS_NAMES = class_info['class_names']
-    BATCH_SIZE = args.batch_size
 
     # Check if tensorflow can access the GPU
     device_name = tf.test.gpu_device_name()
@@ -307,8 +128,31 @@ if __name__ == '__main__':
     else:
         print('Found GPU at: {}'.format(device_name))
 
-    main(args, class_info)
+    print_log("STARTING EXECUTION AT\t{}".format(time.strftime("%d-%m %H:%M:%S")), print_on_screen=True)
+
+    # SELECTING MODELS
+    model_class = _model_selection(args, class_info['n_classes'])
+
+    # Initialize variables and logs
+    modes.initialization(args, class_info, ds_info)
+
+    if args.tuning is not None:
+        modes.tuning(args, model_class, ds_info)
+    elif args.load_model is not None:
+        model = modes.load_model(args)
+        modes.test(args, model, class_info, ds_info)
+    else:
+        model = model_class.build()
+
+        if args.mode == 'training':
+            modes.train_val(args, model, ds_info)
+        elif args.mode == 'test':
+            modes.train_test(args, model, class_info, ds_info)
+
+        modes.save_model(args, model)
+
+    print_log("ENDING EXECUTION AT\t{}".format(time.strftime("%d-%m %H:%M:%S")), print_on_screen=True)
+
     end = time.perf_counter()
     print()
-    print_log("EX. TIME: {} ".format(str(datetime.timedelta(seconds=end-start))), print_on_screen=True)
-
+    print_log("EX. TIME: {} ".format(str(datetime.timedelta(seconds=end - start))), print_on_screen=True)
--- models_base/nedo.py
+++ models_base/nedo.py
@@ -3,9 +3,9 @@ from tensorflow.keras import models
 from tensorflow.keras.metrics import Precision, Recall, AUC
 
 
-class BASIC:
+class NEDO:
 
-    def __init__(self, num_classes, img_size, channels, name="basic"):
+    def __init__(self, num_classes, img_size, channels, name="nedo"):
         self.name = name
         self.num_classes = num_classes
         self.input_width_height = img_size
@@ -13,20 +13,19 @@ class BASIC:
 
     def build(self):
         model = models.Sequential()
-        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(self.input_width_height,
+        model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(self.input_width_height,
                                                                             self.input_width_height,
                                                                             self.channels)))
         model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Conv2D(96, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(128, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Flatten())
-        model.add(layers.Dropout(0.5))  # Dropout for regularization
+        model.add(layers.Dropout(0.45))  # Dropout for regularization
+        model.add(layers.Dense(1024, activation='relu'))
+        model.add(layers.Dropout(0.35))
         model.add(layers.Dense(512, activation='relu'))
-        model.add(layers.Dropout(0.5))
-        model.add(layers.Dense(256, activation='relu'))
-        model.add(layers.Dropout(0.5))
         model.add(layers.Dense(self.num_classes, activation='softmax'))
 
         model.compile(loss='categorical_crossentropy', optimizer='adam',
@@ -40,16 +39,18 @@ class BASIC:
         model.add(layers.Conv2D(hp.Int('filters_1', 16, 128, step=16), (3, 3), activation='relu',
                                 input_shape=(self.input_width_height, self.input_width_height, self.channels)))
         model.add(layers.MaxPooling2D((2, 2)))
-        for i in range(hp.Int('conv_blocks', 1, 2, default=1)):
-            model.add(layers.Conv2D(hp.Int('filters_' + str(i), 32, 64, step=32), (3, 3), activation='relu'))
+        for i in range(hp.Int('conv_blocks', 2, 5, default=3)):
+            model.add(layers.Conv2D(hp.Int('filters_' + str(i), 32, 256, step=32), (3, 3), activation='relu'))
             model.add(layers.MaxPooling2D((2, 2)))
             #if hp.Choice('pooling_' + str(i), ['avg', 'max']) == 'max':
             #    x = tf.keras.layers.MaxPool2D()(x)
             #else:
             #    x = tf.keras.layers.AvgPool2D()(x)
         model.add(layers.Flatten())
-        model.add(layers.Dense(hp.Int('hidden_size', 256, 512, step=128, default=512), activation='relu'))
-        model.add(layers.Dropout(hp.Float('dropout', 0.3, 0.5, step=0.1, default=0.5)))
+        model.add(layers.Dropout(hp.Float('dropout', 0, 0.7, step=0.1, default=0.5)))
+        model.add(layers.Dense(hp.Int('hidden_size', 512, 1024, step=128, default=512), activation='relu'))
+        model.add(layers.Dropout(hp.Float('dropout', 0, 0.7, step=0.1, default=0.5)))
+        model.add(layers.Dense(hp.Int('hidden_size', 128, 512, step=128, default=512), activation='relu'))
         model.add(layers.Dense(self.num_classes, activation='softmax'))
         # activation=hp.Choice('act_1', ['relu', 'tanh'])
 
