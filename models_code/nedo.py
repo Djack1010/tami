73003c0771f72971d8ea45e131c190269dfe0d35
djack1010 <giachi.iada@gmail.com>
2020-11-07 18:41:53 +0100

    edit model architecture

--- models_code/nedo.py
+++ models_code/nedo.py
@@ -16,18 +16,16 @@ class NEDO:
         model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(self.input_width_height,
                                                                             self.input_width_height,
                                                                             self.channels)))
+        model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(96, (3, 3), activation='relu'))
-        model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-        model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Flatten())
-        model.add(layers.Dropout(0.45))  # Dropout for regularization
         model.add(layers.Dense(1024, activation='relu'))
-        model.add(layers.Dropout(0.40))
-        model.add(layers.Dense(512, activation='relu'))
-        model.add(layers.Dropout(0.35))
+        model.add(layers.Dropout(0.45))
         model.add(layers.Dense(256, activation='relu'))
+        model.add(layers.Dropout(0.35))
         model.add(layers.Dense(self.num_classes, activation='softmax'))
 
         model.compile(loss='categorical_crossentropy', optimizer='adam',
