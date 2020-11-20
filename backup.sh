0b6a00f0207746c692a0ded34896add159e263f0
djack1010 <giachi.iada@gmail.com>
2020-11-20 12:15:30 +0100

    integrate backup with cleanup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +%Y%m%d_%H%M)
+if [ "$#" -ne 1 ]; then
+    echo "ERROR: Provide name for folder to store in /BACKUP"
+    echo "USAGE: $0 NAME_FOLDER_TO_CREATE"
+    exit
+fi
+
+#TIMESTAMP=$(date +%Y%m%d_%H%M)
+TIMESTAMP=$1
 
 if [ "$(ls results/exec_logs/)" ]; then
   mkdir -p "BACKUP/${TIMESTAMP}"
@@ -30,6 +37,10 @@ if [ "$(ls models_saved/)" ]; then
   mkdir -p "BACKUP/${TIMESTAMP}"
   cp -r models_saved/ BACKUP/${TIMESTAMP}/
   rm -f models_saved/.gitignore
+fi
+
+if [ -d "BACKUP/${TIMESTAMP}" ]; then
+  echo "BACKUP CREATED on the $(date +%d-%m-%Y) at $(date +%H:%M)" >> BACKUP/${TIMESTAMP}/info.txt
 fi
 
 echo "If not empty, copied results/exec_logs/, results/figures/, results/tensorboard/, tuning/ and models_saved/ to BACKUP/${TIMESTAMP}"
--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-rm -f temp/train.*
-rm -f temp/val.*
-rm -f temp/test.*
-rm -f temp/fin_tr.*
-rm -f temp/*.data
-
-echo "Cleaning /temp folder"
-
 if [ "$1" == "--logs" ]; then
 	rm -f results/exec_logs/*.results
 	rm -f results/figures/*.png
@@ -19,6 +11,18 @@ elif [ "$1" == "--models" ]; then
 elif [ "$1" == "--tuning" ]; then
   rm -rf tuning/*
   echo "Cleaning /tuning folder"
+elif [ "$1" == "--complete" ]; then
+  ./cleanup.sh --logs --ignore
+  ./cleanup.sh --models --ignore
+  ./cleanup.sh --tuning --ignore
 fi
 
-echo "USAGE: $0 --[logs|models|tuning]"
+if [ "$2" != "--ignore" ]; then
+  rm -f temp/train.*
+  rm -f temp/val.*
+  rm -f temp/test.*
+  rm -f temp/fin_tr.*
+  rm -f temp/*.data
+  echo "Cleaning /temp folder"
+  echo "USAGE: $0 --[logs|models|tuning|complete]"
+fi
