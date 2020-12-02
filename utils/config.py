da4587c946660e1cb546ea099d6344e46b191e74
djack1010 <giachi.iada@gmail.com>
2020-12-02 17:47:41 +0100

    -

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 import time
 
 # MAIN Absolute Path
-main_path = "/<INSERT_FULL_PATH_TO_REPO_FOLDER>/tami/"
+main_path = "/home/christian/Documenti/Proj/tami/"
 # Timestamp Execution
 timeExec = "{}".format(time.strftime("%d%m_%H%M"))
 
