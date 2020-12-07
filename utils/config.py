550bf74338e8f68fafae15460590d89564ede897
djack1010 <giachi.iada@gmail.com>
2020-12-07 21:20:49 +0100

    update timestamp with seconds and info of dataset_test

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ import time
 # MAIN Absolute Path
 main_path = ""
 # Timestamp Execution
-timeExec = "{}".format(time.strftime("%d%m_%H%M"))
+timeExec = "{}".format(time.strftime("%d%m_%H%M%S"))
 
 # GLOBAL variables
 AUTOTUNE, CHANNELS, IMG_DIM, CLASS_NAMES, BATCH_SIZE = None, None, None, None, None
