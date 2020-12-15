c68ee3b63ff73a0edc64bef3384494ac53f4a6d4
1Stohk1 <chrispeluso98@gmail.com>
2020-12-15 13:51:08 +0100

    Saving the current project

--- main_cati.py
+++ main_cati.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import re
 import time
+import subprocess
+
 
 from utils.config import timeExec
 import cati.utils.opcode as opcode
@@ -23,7 +25,7 @@ def parse_args():
     group.add_argument('-i', '--image_size', required=False, type=str, default="250x1",
                        help='FORMAT ACCEPTED = SxC , the Size (SIZExSIZE) and channel of the images in input '
                             'default is [250x1] (reshape will be applied)')
-    group.add_argument('-o', '--output_name', required=False, type=str, default="date",
+    group.add_argument('-o', '--output_name', required=False, type=str, default="data",
                        help='Enter the name by which you want to call the output')
     # FLAGS
     group.add_argument('--no_storage', dest='storage', action='store_false',
@@ -65,9 +67,9 @@ if __name__ == "__main__":
     apk = {}
     converter = opcode.Converter()
 
-    if not os.path.isdir(f"{RESULTS}/{args.output_name}_{timeExec}"):
-        tools.create_folder(f"{RESULTS}/{args.output_name}_{timeExec}")
-    RESULTS = f"{RESULTS}/{args.output_name}_{timeExec}"
+    if not os.path.isdir(f"{RESULTS}/{args.output_name}"):
+        tools.create_folder(f"{RESULTS}/{args.output_name}")
+    RESULTS = f"{RESULTS}/{args.output_name}"
 
     for family in os.listdir(DECOMPILED):
         if os.path.isdir(f'{DECOMPILED}/{family}'):
@@ -83,51 +85,57 @@ if __name__ == "__main__":
                     for subdirectory in os.listdir(smali_folder):
                         if "assets" not in subdirectory and "original" not in subdirectory and "res" not in subdirectory:
                             tools.find_smali(f"{smali_folder}/{subdirectory}", smali_paths)
-
-                    general_content = ""
-                    class_legend = ""
-                    smali_k = {}
-                    close = 0
-                    for smali in smali_paths:
-                        class_name = smali.replace(f'{DECOMPILED}/{family}', "")
-
-                        fr = open(smali, "r")
-                        encoded_content = converter.encoder(fr.read())
-                        fr.close()
-
-                        # calculating number of lines and characters of the encoded class
-                        num_line = encoded_content.count('\n')
-                        num_character = len(encoded_content)
-
-                        # saving number of character and coordinates of begin and finish of the class
-                        smali_k[class_name] = num_character
-                        begin = close + 1
-                        close = begin + num_line
-                        class_legend += f"{class_name} [{begin},{close}]\n"
-                        general_content += encoded_content
-
-                    # creating the image on the whole converted text
-                    if args.channels == 1:
-                        img, pix_map, dim = image.img_generator(general_content, True)
-                        image.char_reader_greyscale(general_content, pix_map, dim)
+                    if not smali_paths:
+                        print(f'ciao {smali_folder}/{subdirectory}')
+                        subprocess.call([f'rm -r {DECOMPILED}/{family}/{file}'],
+                                        stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
                     else:
-                        img, pix_map, dim = image.img_generator(general_content, False)
-                        image.char_reader_colorful(general_content, pix_map, dim)
+                        print('bello')
+                        pass
+                        general_content = ""
+                        class_legend = ""
+                        smali_k = {}
+                        close = 0
+                        for smali in smali_paths:
+                            class_name = smali.replace(f'{DECOMPILED}/{family}', "")
 
-                    img.save(f"{RESULTS}/{family}/{file}.png")
+                            fr = open(smali, "r")
+                            encoded_content = converter.encoder(fr.read())
+                            fr.close()
 
-                    img, pix_map, dim = image.legend_image_generator(len(general_content))
-                    image.legend_pixel_generator(smali_k, pix_map, dim)
-                    img.save(f"{RESULTS}/{family}/{file}_legend.png")
+                            # calculating number of lines and characters of the encoded class
+                            num_line = encoded_content.count('\n')
+                            num_character = len(encoded_content)
 
-                    # saving the .txt containing all the compressed classes
-                    tools.save_txt(f"{RESULTS}/{family}/{file}.txt", general_content, True)
+                            # saving number of character and coordinates of begin and finish of the class
+                            smali_k[class_name] = num_character
+                            begin = close + 1
+                            close = begin + num_line
+                            class_legend += f"{class_name} [{begin},{close}]\n"
+                            general_content += encoded_content
 
-                    # saving the legend of the classes
-                    tools.save_txt(f"{RESULTS}/{family}/{file}_legend.txt", class_legend, False)
+                        # creating the image on the whole converted text
+                        if args.channels == 1:
+                            img, pix_map, dim = image.img_generator(general_content, True)
+                            image.char_reader_greyscale(general_content, pix_map, dim)
+                        else:
+                            img, pix_map, dim = image.img_generator(general_content, False)
+                            image.char_reader_colorful(general_content, pix_map, dim)
 
-                    # saving the legend of the classes in the image
-                    tools.save_txt(f"{RESULTS}/{family}/{file}_PNG_Legend.txt", image.legend_of_image(dim, smali_k), True)
+                        img.save(f"{RESULTS}/{family}/{file}.png")
+
+                        img, pix_map, dim = image.legend_image_generator(len(general_content))
+                        image.legend_pixel_generator(smali_k, pix_map, dim)
+                        img.save(f"{RESULTS}/{family}/{file}_legend.png")
+
+                        # saving the .txt containing all the compressed classes
+                        tools.save_txt(f"{RESULTS}/{family}/{file}.txt", general_content, True)
+
+                        # saving the legend of the classes
+                        tools.save_txt(f"{RESULTS}/{family}/{file}_legend.txt", class_legend, False)
+
+                        # saving the legend of the classes in the image
+                        tools.save_txt(f"{RESULTS}/{family}/{file}_PNG_Legend.txt", image.legend_of_image(dim, smali_k), True)
 
     if args.storage:
         process_data.create_dataset(apk, args.output_name, args.image_size,
