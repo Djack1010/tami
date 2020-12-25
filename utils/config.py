f36e6e93a95ac2a0441e3f90860711d71bee9348
Christian Peluso <63061198+1Stohk1@users.noreply.github.com>
2020-12-25 20:47:43 +0100

    Update config.py
    Removing path

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 import time
 
 # MAIN Absolute Path
-main_path = "/home/christian/Documenti/Proj/tami/"
+main_path = ""
 # Timestamp Execution
 timeExec = "{}".format(time.strftime("%d%m_%H%M%S"))
 
