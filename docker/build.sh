9d83d43dc862830a4060731417da50418b09e474
djack1010 <giachi.iada@gmail.com>
2021-12-15 17:55:55 +0100

    remove sudo from docker commands, update run_container.sh with '--no-gpu' input for running on CPU

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 cp ../requirements.txt .
 
-sudo docker build -t tami_exp/tensorflow:latest .
+docker build -t tami_exp/tensorflow:latest .
 
 rm -f requirements.txt
--- docker/run_container.sh
+++ docker/run_container.sh
@@ -2,4 +2,8 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTPATH
 
-docker run --gpus all --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp/tensorflow:latest bash
+if [ "$1" == "--no-gpu" ]; then
+  docker run --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp/tensorflow:latest bash
+else
+  docker run --gpus all --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp/tensorflow:latest bash
+fi
