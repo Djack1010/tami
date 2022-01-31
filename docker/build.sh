f8d37eeb481d140e3dece8169d99f3215ff8c9e2
djack1010 <giachi.iada@gmail.com>
2022-01-31 12:59:14 +0100

    add QCCN_QConv model

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,12 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTPATH
-cp ../requirements.txt .
+
+if [ "$1" == "--quantum" ]; then
+  cp ../quantum_requirements.txt ./requirements.txt
+else
+  cp ../requirements.txt .
+fi
 
 docker build -t tami_exp/tensorflow:latest .
 
