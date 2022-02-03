a16ecb8504eeff0bef7992d93169c748081ad8ee
djack1010 <giachi.iada@gmail.com>
2022-02-03 19:48:30 +0100

    update docker script with quantum/standard model settings

--- docker/build.sh
+++ docker/build.sh
@@ -1,13 +1,29 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+function usage {
+    echo "USAGE: $0 [--quantum]"
+    exit
+}
+
 cd $SCRIPTPATH
 
-if [ "$1" == "--quantum" ]; then
-  cp ../quantum_requirements.txt ./requirements.txt
-else
-  cp ../requirements.txt .
-fi
+QUANTUM=0
 
-docker build -t tami_exp/tensorflow:latest .
+for arg in "$@"; do
+    if [ "$arg" == "--quantum" ]; then
+        QUANTUM=1
+    else
+        echo "ERROR! Incorrect parameter '$arg'"
+        usage 
+    fi 
+done
+
+if (($QUANTUM)); then
+    cp ../full_requirements.txt ./requirements.txt
+    docker build -t tami_exp_quantum/tensorflow:latest .
+else
+    cp ../partial_requirements.txt ./requirements.txt
+    docker build -t tami_exp/tensorflow:latest .
+fi
 
 rm -f requirements.txt
--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,9 +1,36 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+function usage {
+  echo "USAGE: $0 [--quantum] [--no-gpu]"
+  exit
+}
+
 cd $SCRIPTPATH
 
-if [ "$1" == "--no-gpu" ]; then
-  docker run --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp/tensorflow:latest bash
+QUANTUM=0
+GPU=1
+
+for arg in "$@"; do
+  if [ "$arg" == "--quantum" ]; then
+    QUANTUM=1
+  elif [ "$arg" == "--no-gpu" ]; then
+    GPU=0
+  else
+    echo "ERROR! Incorrect parameter '$arg'"
+    usage 
+  fi 
+done
+
+if (($QUANTUM)); then
+  if (($GPU)); then
+    docker run --gpus all --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp_quantum/tensorflow:latest bash
+  else
+    docker run --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp_quantum/tensorflow:latest bash
+  fi
 else
-  docker run --gpus all --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp/tensorflow:latest bash
+  if (($GPU)); then
+    docker run --gpus all --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp/tensorflow:latest bash
+  else
+    docker run --rm -u $(id -u):$(id -g) -v $(pwd)/..:/home/tami -it tami_exp/tensorflow:latest bash
+  fi
 fi
