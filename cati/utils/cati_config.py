bcf8215b42d3ae7455c0e67e417a7f62b1c0198b
djack1010 <giachi.iada@gmail.com>
2022-02-08 09:09:58 +0100

    fix path cati_config.py

--- cati/utils/cati_config.py
+++ cati/utils/cati_config.py
@@ -1,4 +1,9 @@
 from utils.config import main_path, timeExec
+import os
+
+# SET main_path at runtime with the absolute path of the project root folder
+path_list = os.path.realpath(__file__).split("/")[:-3]
+main_path = '/'.join(path_list) + '/'
 
 APK_DIR = f"{main_path}cati/sample"
 DECOMPILED = f"{main_path}cati/decompiled"
