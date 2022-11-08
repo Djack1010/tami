9703731b3d1c6b864e02d1b229d5c1e05e35d515
djack1010 <giachi.iada@gmail.com>
2022-11-08 11:15:55 +0100

    update Tami pre-built image URL

--- docker/download_and_load_image.sh
+++ docker/download_and_load_image.sh
@@ -1,8 +1,13 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPTPATH}
 
+URLTAMI="https://martellone.iit.cnr.it/index.php/s/8qA8jtiK5SYtMk9/download/tami_expV1.0.tar"
+
 #docker save -o <path for generated tar file> <image name>
-wget "https://ocsdev-cloud.duckdns.org/index.php/s/JJpd7CiNJryYQzG/download/tami_expV1.0"
-docker load -i "tami_expV1.0"
+echo "Downloading TAMI image from ocsdev cloud"
+wget ${URLTAMI}
+echo "Loading TAMI image in local docker instance (it may take a while...)"
+NAMETAMI=$(basename ${URLTAMI})
+docker load -i ${NAMETAMI}
 
 echo "Load completed, you can now run run_container.sh"
