bdf3415ffbd0667d312e63e2c1641e91917b6657
djack1010 <giachi.iada@gmail.com>
2022-11-24 16:20:04 +0100

    update version

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ main_path = None
 # Timestamp Execution
 timeExec = "{}".format(time.strftime("%d%m_%H%M%S"))
 
-__version__ = "1.3"  # Updated on the 20/07/2022
+__version__ = "1.5"  # Updated on the 24/11/2022
 
 # GLOBAL variables
 AUTOTUNE, CHANNELS, IMG_DIM, VECTOR_DIM, CLASS_NAMES, BATCH_SIZE, DATA_REQ, LEARNING_RATE, WEIGHTS = \
